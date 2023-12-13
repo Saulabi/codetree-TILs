54ac132930ba2766980db144894b6abef0769815
Byounggwan Yu <ybg4523@gmail.com>
2023-12-13 17:10:38 +0900

    Title: 비교 연산, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231213/비교 연산/comparison-operator.py
@@ -0,0 +1,38 @@
+# 두 정수 a, b 입력 받기
+a, b = map(int, input().split())
+
+# a가 b보다 같거나 큰가?
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+# a가 b보다 큰가?
+if a > b:
+    print(1)
+else:
+    print(0)
+
+# b가 a보다 같거나 큰가?
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+# b가 a보다 큰가?
+if b > a:
+    print(1)
+else:
+    print(0)
+
+# a와 b가 같은가?
+if a == b:
+    print(1)
+else:
+    print(0)
+
+# a와 b가 다른가?
+if a != b:
+    print(1)
+else:
+    print(0)
