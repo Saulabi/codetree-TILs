a16707b5f799a490ecb241b7ea798f0506c070c8
Byounggwan Yu <ybg4523@gmail.com>
2023-12-14 14:23:00 +0900

    Title: 두 사람, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231214/두 사람/two-person.py
+++ 231214/두 사람/two-person.py
@@ -1,4 +1,10 @@
 p1 = input().split()
 p2 = input().split()
 
-print(int(((int(p1[0])>= 19) or (int(p2[0]) >= 19)) and ((p1[1]== 'M') or (p2[1] == 'M'))))
+if (int(p1[0]) >= 19) or (int(p2[0]) >= 19):
+    if (p1[1]== 'M') or (p2[1] == 'M'):
+        print('1')
+    else:
+        print('0')
+else:
+    print('0')
