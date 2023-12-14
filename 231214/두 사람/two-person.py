1941ca73e879d3eb0cc0e3ddc4a3938c0dd1ae33
Byounggwan Yu <ybg4523@gmail.com>
2023-12-14 15:36:58 +0900

    Title: 두 사람, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 231214/두 사람/two-person.py
+++ 231214/두 사람/two-person.py
@@ -1,4 +1,4 @@
 p1 = input().split()
 p2 = input().split()
 
-print(int((int(p1[0]) >= 19 or int(p2[0]) >= 19) and (p1[1] == 'M' or p2[1] == 'M')))
+print(int((int(p1[0]) >= 19 and p1[1] == 'M') or (int(p2[0]) >= 19 and p2[1] == 'M')))
