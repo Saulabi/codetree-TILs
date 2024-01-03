ed4927cf8cfc9da0d649c8169efcb9d43d7d8956
Byounggwan Yu <ybg4523@gmail.com>
2024-01-03 16:33:43 +0900

    Title: 조건에 따른 연산, Time: 131ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240103/조건에 따른 연산/operatino-by-rule.py
@@ -0,0 +1,16 @@
+def get_cnt(n):
+    cnt = 0
+
+    while n < 1000:
+        if n % 2 == 0:
+            n = n * 3 + 1
+        else:
+            n = n * 2 + 2
+        cnt += 1
+
+    return cnt
+
+n = int(input())
+
+result = get_cnt(n)
+print(result)
