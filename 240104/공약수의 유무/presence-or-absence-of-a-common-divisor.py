5bba0c9427133f31be4b2adb40878fe0daec677f
Byounggwan Yu <ybg4523@gmail.com>
2024-01-04 17:50:08 +0900

    Title: 공약수의 유무, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240104/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240104/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -3,7 +3,8 @@ def find_common_divisor(a, b):
     for i in range(a, b+1):
         if 1920 % i == 0 or 2880 % i == 0:
             return 1  # 공약수가 존재하는 경우
-        return 0  # 공약수가 존재하지 않는 경우
+        else:
+            return 0  # 공약수가 존재하지 않는 경우
 
 # 입력 받기
 a, b = map(int, input().split())
