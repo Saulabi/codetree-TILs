847ff776e60df79c79906953505908695183d457
Byounggwan Yu <ybg4523@gmail.com>
2024-01-04 17:47:08 +0900

    Title: 공약수의 유무, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240104/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,13 @@
+def find_common_divisor(a, b):
+    # a 이상 b 이하의 수 중에서 1,920과 2,880의 공약수를 찾는 함수
+    for i in range(a, b+1):
+        if 1920 % i == 0 or 2880 % i == 0:
+            return 1  # 공약수가 존재하는 경우
+    return 0  # 공약수가 존재하지 않는 경우
+
+# 입력 받기
+a, b = map(int, input().split())
+
+# 결과 출력
+result = find_common_divisor(a, b)
+print(result)
