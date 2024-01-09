11900a667f991d1e778715a251610880595499cd
Byounggwan Yu <ybg4523@gmail.com>
2024-01-09 18:19:51 +0900

    Title: 공약수의 유무, Time: 147ms, Memory: 73MB, Status: Passed - CodeTree

--- 240109/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 240109/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -1,13 +1,15 @@
 a,b = map(int,input().split())
-gong = []
-for i in range(2,1920):
-    if 1920 % i == 0 and 2880 % i ==0:
-        gong.append(i)
-sati = False
-for j in range(len(gong)):
-    if gong[j] >=a and gong[j] <=b:
-        sati = True
-if sati == True:
-    print(1)
+
+satisfied = False
+
+for i in range(a, b + 1):
+	# a에서 b사이의 값 중 공약수가 있는지 확인합니다.
+	if 1920 % i == 0 and 2880 % i == 0:
+		satisfied = True
+
+
+# 출력
+if satisfied == True:
+	print("1")
 else:
-    print(0)
+	print("0")
