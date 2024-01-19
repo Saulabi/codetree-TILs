dcc12ee02d4866cef7906c58a18568bd4a589cea
Byounggwan Yu <ybg4523@gmail.com>
2024-01-19 22:06:53 +0900

    Title: 별표 출력하기 10, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240119/별표 출력하기 10/print-star-10.py
+++ 240119/별표 출력하기 10/print-star-10.py
@@ -10,3 +10,14 @@ for i in range(2 * n):
 		for _ in range(n - (i - 1) // 2):
 			print("*", end=" ")
 	print()
+
+
+# n = int(input())
+
+# for i in range(1, n + 1):
+#     for _ in range(i):
+#         print("* ", end="")
+#     print()
+#     for _ in range(n - i + 1):
+#         print("* ", end="")
+#     print()
