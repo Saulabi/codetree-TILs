ec503818d9563ba14beeb45acc2092b821918e6f
Byounggwan Yu <ybg4523@gmail.com>
2024-02-07 21:52:47 +0900

    Title: 순서쌍 만들기, Time: 149ms, Memory: 24MB, Status: Passed - CodeTree

--- 240207/순서쌍 만들기/making-order-pair.py
+++ 240207/순서쌍 만들기/making-order-pair.py
@@ -4,3 +4,13 @@ for i in range(n, 0, -1):
     for j in range(n, 0, -1):
         print(f'({i},{j})', end=' ')
     print()
+
+
+# # 변수 선언 및 입력
+# n = int(input())
+	
+# # n칸의 정사각형에 올바른 순서쌍을 출력합니다.
+# for i in range(n):
+# 	for j in range(n):
+# 		print(f"({n - i},{n - j})", end=" ")
+# 	print()
